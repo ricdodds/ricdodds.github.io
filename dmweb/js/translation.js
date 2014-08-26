b869d8ab8a01afbd1537f034476a95aa678e707b
ricdodds <ric.dodds@gmail.com>
2014-08-26 15:16:43 -0400

    Translation

--- dmweb/js/translation.js
+++ dmweb/js/translation.js
@@ -1,9 +1,20 @@
 ﻿$(document).ready(function () {
-  language = 'en';
+  language = 'es';
 
-  i18n.init({ lng: language, debug: true }, function () {
+  i18n.init({lng: language}, function () {
     // save to use translation function as resources are fetched
     $('.nav').i18n();
-    //$("headline").i18n();
+  });
+});
+
+$('.english-switch').click(function () {
+  i18n.setLng('en', function (t) {
+    $('.nav').i18n();
+  });
+});
+
+$('.spanish-switch').click(function () {
+  i18n.setLng('es', function (t) {
+    $('.nav').i18n();
   });
 });
