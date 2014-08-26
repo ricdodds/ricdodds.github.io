19c551719c36566784d254fe181e02e10b9f8519
ricdodds <ric.dodds@gmail.com>
2014-08-26 13:12:33 -0400

    Translation

---
+++ dmweb/js/translation.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+  language = 'en';
+
+  i18n.init({ lng: language, debug: true }, function () {
+    // save to use translation function as resources are fetched
+    $('.nav').i18n();
+    //$("headline").i18n();
+  });
+});
