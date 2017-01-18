f227a270fb1b986f9455b006499532e8ad2ce1ed
ricdodds <ric.dodds@gmail.com>
2017-01-18 00:01:04 -0500

    implemented freehand line

--- sol-lewitt/sol-lewitt.js
+++ sol-lewitt/sol-lewitt.js
@@ -17,8 +17,8 @@ function setup() {
 
 	for (row = 0; row < ysize; row++) {
 		for (column = 0; column < xsize; column++) {
-			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 4));
-			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 4));
+			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 5));
+			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 5));
 		}
 	}
 }
@@ -36,6 +36,9 @@ function drawShape(x, y, index, group) {
 			break;
 		case 4:
 			drawDashedLine(x, y, index);
+			break;
+		case 5:
+			drawFreehandLine(x, y, index);
 			break;
 	}
 }
@@ -99,7 +102,7 @@ function drawDashedLine(x, y, index) {
 			dashedLine(x * r, y * r, x * r + r, y * r + r);
 			break;
 		case 2:
-			dashedLine(x * r, y * r + r, x * r + r, y * r);
+			dashedLine(x * r, y * r, x * r + r, y * r + r);
 			break;
 		case 3:
 			dashedLine(x * r + r/2, y * r, x * r + r/2, y * r + r);
@@ -146,5 +149,63 @@ function dashedLine(x0, y0, x1, y1) {
 			skip = false;
 		}
 		y += dy;
+	}
+}
+
+function drawFreehandLine(x, y, index) {
+	switch(index) {
+		case 1:
+			freehandLine(x * r, y * r, x * r + r, y * r + r);
+			break;
+		case 2:
+			freehandLine(x * r, y * r, x * r + r, y * r + r);
+			break;
+		case 3:
+			freehandLine(x * r + r/2, y * r, x * r + r/2, y * r + r);
+			break;
+		case 4:
+			freehandLine(x * r, y * r + r/2, x * r + r, y * r + r/2);
+			break;
+	}
+
+}
+
+function freehandLine(x0, y0, x1, y1) {
+	var size = 10;
+	var random = 4;
+
+	var steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
+	if(steep) {
+		var tmp;
+		tmp = x0; x0 = y0; y0 = tmp;
+		tmp = x1; x1 = y1; y1 = tmp;
+	}
+
+	var sign = 1;
+	if(x0 > x1) {
+		sign = -1;
+		x0 *= -1;
+		x1 *= -1;
+	}
+
+	var length = Math.sqrt(Math.pow(y1 - y0, 2) + Math.pow(x1 - x0, 2))
+	var step = length / size;
+
+	var dx0 = (x1 - x0) / step;
+	var dy0 = Math.abs(y1 - y0) / step;
+	
+	var y = y0;
+	var x = x0;
+	var dx = dx0;
+	var dy = dy0;
+	
+	while (x < x1 && y < y1) {
+		dx += (dx0 - dx) + (Math.random() * random - random / 2);
+		dy += (dy0 - dy) + (Math.random() * random - random / 2);
+
+		if(!(steep ? line(y, sign * x, y + dy, sign * x + dx) : line(sign * x, y, sign * x + dx, y + dy)))
+			return;
+		x += dx;
+		y += dy
 	}
 }
