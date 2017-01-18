5510f6ee416434d4739c1d9c30f0f3c8cdab7fdf
ricdodds <ric.dodds@gmail.com>
2017-01-17 22:48:43 -0500

    implemented dashed lines

--- sol-lewitt/sol-lewitt.js
+++ sol-lewitt/sol-lewitt.js
@@ -1,6 +1,9 @@
 var r = 200;
 var d = 2 * r;
 
+var xsize = 10;
+var ysize = 5;
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -8,33 +11,36 @@ function getRandomIntInclusive(min, max) {
 }
 
 function setup() {
-	createCanvas(4 * r, 3 * r);
+	createCanvas(xsize * r, ysize * r);
 
 	noFill();
 
-	for (row = 0; row < 3; row++) {
-		for (column = 0; column < 4; column++) {
-			draw_shape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 3));
-			draw_shape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 3));
+	for (row = 0; row < ysize; row++) {
+		for (column = 0; column < xsize; column++) {
+			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 4));
+			drawShape(column, row, getRandomIntInclusive(1, 4), getRandomIntInclusive(1, 4));
 		}
 	}
 }
 
-function draw_shape(x, y, index, group) {
+function drawShape(x, y, index, group) {
 	switch(group) {
 		case 1:
-			draw_quarter(x, y, index);
+			drawQuarter(x, y, index);
 			break;
 		case 2:
-			draw_arc(x, y, index);
+			drawArc(x, y, index);
 			break;
 		case 3:
-			draw_line(x, y, index);
+			drawLine(x, y, index);
+			break;
+		case 4:
+			drawDashedLine(x, y, index);
 			break;
 	}
 }
 
-function draw_quarter(x, y, index) {
+function drawQuarter(x, y, index) {
 	switch(index) {
 		case 1:
 			arc(x * r + r, y * r + r, d, d, PI, -HALF_PI, OPEN);
@@ -52,7 +58,7 @@ function draw_quarter(x, y, index) {
 	
 }
 
-function draw_arc(x, y, index) {
+function drawArc(x, y, index) {
 	switch(index) {
 		case 1:
 			arc(x * r + r * 0.366 + r, y * r + 1/2 * r, d, d, 5/3 * HALF_PI, 7/3 * HALF_PI, OPEN);
@@ -69,7 +75,7 @@ function draw_arc(x, y, index) {
 	}	
 }
 
-function draw_line(x, y, index) {
+function drawLine(x, y, index) {
 	switch(index) {
 		case 1:
 			line(x * r, y * r, x * r + r, y * r + r);
@@ -85,4 +91,60 @@ function draw_line(x, y, index) {
 			break;
 	}
 
+}
+
+function drawDashedLine(x, y, index) {
+	switch(index) {
+		case 1:
+			dashedLine(x * r, y * r, x * r + r, y * r + r);
+			break;
+		case 2:
+			dashedLine(x * r, y * r + r, x * r + r, y * r);
+			break;
+		case 3:
+			dashedLine(x * r + r/2, y * r, x * r + r/2, y * r + r);
+			break;
+		case 4:
+			dashedLine(x * r, y * r + r/2, x * r + r, y * r + r/2);
+			break;
+	}
+
+}
+
+function dashedLine(x0, y0, x1, y1) {
+	var size = 20;
+
+	var steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
+	if(steep) {
+		var tmp;
+		tmp = x0; x0 = y0; y0 = tmp;
+		tmp = x1; x1 = y1; y1 = tmp;
+	}
+
+	var sign = 1;
+	if(x0 > x1) {
+		sign = -1;
+		x0 *= -1;
+		x1 *= -1;
+	}
+
+	var length = Math.sqrt(Math.pow(y1 - y0, 2) + Math.pow(x1 - x0, 2))
+	var step = length / size;
+
+	var dx = (x1 - x0) / step;
+	var dy = Math.abs(y1 - y0) / step;
+	
+	var y = y0;
+	var skip = false;
+
+	for(var x = x0; x < x1; x += dx) {
+		if (!skip) {
+			skip = true;
+			if(!(steep ? line(y, sign * x, y + dy, sign * x + dx) : line(sign * x, y, sign * x + dx, y + dy)))
+				return;
+		} else {
+			skip = false;
+		}
+		y += dy;
+	}
 }
